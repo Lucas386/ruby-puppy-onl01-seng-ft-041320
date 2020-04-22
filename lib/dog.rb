9103dd5a90a2607198cb930588bdb7140157cc87
Lucas Easley <lucaseasley11@gmail.com>
2020-04-22 21:12:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,22 @@
-class Dog
-
-	@@all = []
-	attr_accessor :name
-
-	def initialize(name)
-		@name = name
-		@@all << self
-	end
-
-	def self.clear_all
-		@@all.clear
-	end
-
-	def self.all
-		@@all.each do |dog|
-			puts dog.name
-		end
-	end
-end
+class Dog 
+  attr_accessor :name, 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self #<--- replace this with save
+     end 
+  def self.all 
+    @@all 
+  end 
+  def self.clear_all 
+    @@all.clear
+  end 
+  def self.print_all 
+    @@all.each do |dog| 
+      puts dog.name
+    end 
+  end 
+  def save
+    @@all << self
+  end 
+end 
